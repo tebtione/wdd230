ccb2a0ce654f05b532e386964dfb47de35adfd35
tebtione <97209453+tebtione@users.noreply.github.com>
2025-02-02 07:50:25 -0700

    Make some changes, completed and updated

--- chamber/scripts/hamburger.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const menu = document.getElementById('menu');
-
-hamburger.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    hamburger.innerHTML = menu.classList.contains('active') ? '✖' : '≡';
-});
