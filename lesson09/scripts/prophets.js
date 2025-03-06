ece2aa22e131f3a8d3ef4182e26f48722198c6d4
tebtione <97209453+tebtione@users.noreply.github.com>
2025-03-06 13:06:46 -0700

    Comnpleted and updated

---
+++ lesson09/scripts/prophets.js
@@ -0,0 +1,53 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+const getProphetData = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.table(data); // Check the data response
+    // Call the displayProphets function with the prophets array
+    displayProphets(data.prophets);
+};
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        const card = document.createElement('section');
+        card.classList.add('card');
+
+        const fullName = document.createElement('h2');
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+
+        const portrait = document.createElement('img');
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        const birthdate = document.createElement("p");
+        birthdate.textContent = `Birthdate: ${prophet.birthdate}`;
+
+        const death = document.createElement("p");
+        death.textContent = `Death: ${prophet.death ? prophet.death : 'N/A'}`;
+
+        const birthplace = document.createElement("p");
+        birthplace.textContent = `Birthplace: ${prophet.birthplace}`;
+
+        const numOfChildren = document.createElement("p");
+        numOfChildren.textContent = `Number of Children: ${prophet.numofchildren}`;
+
+        // Append all elements to the card
+        card.appendChild(fullName);
+        card.appendChild(portrait);
+        card.appendChild(birthdate);
+        card.appendChild(death);
+        card.appendChild(birthplace);
+        card.appendChild(numOfChildren);
+
+        // Append the card to the cards container
+        cards.appendChild(card);
+    });
+};
+
+// Call the function to fetch and display the data
+getProphetData();
