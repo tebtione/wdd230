58bdd9f570280cab7d8e6bad6935e91e58981139
tebtione <97209453+tebtione@users.noreply.github.com>
2025-03-08 18:38:32 -0700

    Completed and updated

--- scripts/link.js
+++ scripts/link.js
@@ -1,5 +1,5 @@
 const baseURL = "https://tebtione.github.io/wdd230/";
-const linlsURL = "https://tebtione.github.io/wdd230/data/links.json";
+const linksURL = "https://tebtione.github.io/wdd230/data/links.json";
 
 async function getLinks() {
     const response = await fetch(linksURL);
